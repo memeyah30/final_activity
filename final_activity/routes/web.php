94434b0828228b9450539eff2d81c78e9897ff14
memeyah30 <sapanruffa675@gmail.com>
2025-06-06 13:23:44 +0800

    Update and Delete Route

--- final_activity/routes/web.php
+++ final_activity/routes/web.php
@@ -2,6 +2,12 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [ProductController::class, 'index']);
 Route::get('/products/create', [ProductController::class, 'create']);
 Route::post('/products', [ProductController::class, 'store']);
+
+// ➕ Add these:
+Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
+Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::delete('/products/{id}', [ProductController::class, 'destroy']);
